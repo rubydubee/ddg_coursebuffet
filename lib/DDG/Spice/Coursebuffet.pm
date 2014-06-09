1763677d79250e8ae89268ede0bc7f59b31dab94
Pradyumna <don.paddy@gmail.com>
2014-06-08 18:06:00 -0700

    Comply with new DDG changes

--- lib/DDG/Spice/Coursebuffet.pm
+++ lib/DDG/Spice/Coursebuffet.pm
@@ -1,42 +1,47 @@
 package DDG::Spice::Coursebuffet;
-# ABSTRACT: Give the number of characters (length) of the query.
 
 use DDG::Spice;
-use File::Slurp;
-use Switch;
 
-my @course_titles = read_file('../../share/spice/coursebuffet/course_titles.txt');
-
-triggers any => @course_titles;
-triggers any => 'course','edx', 'coursera', 'udacity', 'saylor';
-
-spice to => 'http://coursebuffet.com/ddg/$1';
+spice to => 'http://www.coursebuffet.com/ddg/$1/$2';
+spice from => '(.*?)/(.*)';
 spice wrap_jsonp_callback => 1;
 
-handle remainder => sub {
-  $text = $_;
+primary_example_queries "computer science course";
+secondary_example_queries "computer science coursera";
+description "Course catalog for online learning!";
+name "CourseBuffet";
 
-  # Case for provider specific queries
-  $text =~ /(coursera|edx|udacity|saylor)/;
-  if($&) {
-    $provider = $&;
-    $query = $';
-    $query =~ s/^\s+|\s+$//g;
-    $query = uri_escape($query);
-    return "provider/$provider/$query";
+my @title_list = share('course_titles.txt')->slurp;
+my @course_titles = ();
+foreach my $title (@title_list) {
+  chomp $title;
+  push(@course_titles, $title);
+}
+
+triggers query_lc => qr/\s*/;
+
+handle query_lc => sub {
+
+  $_ =~ /(coursera|edx|udacity|saylor|novoed|futurelearn|iversity|open2study|openuped)/;
+  if ($&) {
+    return "provider", $&, "$` $'";
   }
 
-  # Case for keyword 'course'
-  $text =~ /course/;
-  if($&) {
-    $query = "";
-    $query += $` if $`;
-    $query += $' if $';
-    $query =~ s/^\s+|\s+$//g;
-    $query = uri_escape($query);
-    return "provider/$provider/$query";
+  $_ =~ /course/;
+  if ($&) {
+    return "standard","courses", "$` $'";
   }
 
-  return;
+  my $query = $_;
+
+  my @matches = grep { /^$query$/i } @course_titles;
+  
+  if(@matches) {
+    return "title", "courses" ,$matches[0];
+  } else {
+    return;
+  }
+
 };
+
 1;
